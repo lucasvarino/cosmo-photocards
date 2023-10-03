8e5bbe38e74c6ff9bd13ef864a75ee171083832c
Lucas Varino <lucasvarino@gmail.com>
2023-10-03 20:25:05 -0300

    refactor: Refactoring the file objekt to lib folder and adding some methods

--- lib/objekts.ts
+++ lib/objekts.ts
@@ -47,6 +47,51 @@ type RawObjekt = {
   };
 };
 
+export async function fetchObjektsForOwner({
+  address,
+  contracts,
+  orderBy,
+  pageKey,
+}: ObjektRequest): Promise<ObjektResponse> {
+  const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
+  const endpoint = `curl 'https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}/getNFTs/`;
+  const params = new URLSearchParams({
+    owner: address,
+    withMetadata: "true",
+    pageSize: "100",
+  });
+
+  contracts.forEach((contract) =>
+    params.append("contractAddresses[]", contract)
+  );
+
+  if (orderBy) {
+    params.append("orderBy", orderBy);
+  }
+
+  if (pageKey) {
+    params.append("pageKey", pageKey);
+  }
+
+  try {
+    const response = await fetch(endpoint + "?" + params.toString(), {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    });
+
+    const data = await response.json();
+    return {
+      ownedNfts: data.ownedNfts ?? [],
+      pageKey: data.pageKey ?? undefined,
+      totalCount: parseInt(data.totalCount),
+    };
+  } catch (err) {
+    throw new Error("Unable to fetch objekts");
+  }
+}
+
 const ADDRESSES = [
   "0xA4B37bE40F7b231Ee9574c4b16b7DDb7EAcDC99B", // tripleS
   "0x0fB69F54bA90f17578a59823E09e5a1f8F3FA200", // ARTMS
